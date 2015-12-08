4a61e8ac658ec152970aabe7fa45ad00223216a7
ally <ally@dibs.dk>
2015-12-08 17:03:53 +0200

    fix conflicts after rebase

--- src/test/java/driverLogic/TestBase.java
+++ src/test/java/driverLogic/TestBase.java
@@ -1,6 +1,5 @@
 package driverLogic;
 
-
 import ServicePages.CriminalRecordPage;
 import ServicePages.InternationalPassportPage;
 import ServicePages.SubsidyPage;
@@ -15,6 +14,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import pages.*;
 
 @Listeners({appLogic.ScreenshotListener.class})
 public class TestBase {
@@ -67,92 +67,4 @@ public class TestBase {
         unregisterFromLocationPage = new UnregisterFromLocationPage(driver);
         driver.get(Constants.Server.VersionSERVER);
     }
-
-//    @AfterMethod(alwaysRun = true)
-//    public void takeScreenshot(ITestResult result) throws Exception
-//    {
-//        //Для того чтобы передавать html теги и спец-символы в reporter.log
-//        //Или можно передать параметр в командную строку при выполнении TestNG:
-//        //-Dorg.uncommons.reportng.escape-output=false
-//        System.setProperty("org.uncommons.reportng.escape-output", "false");
-//
-//        Reporter.setCurrentTestResult(result);
-//
-//        //Create a directory; all non-existent ancestor directories are
-//        //automatically created
-//        boolean success = (new File("TestReport/html/Screens/")).mkdirs();
-//        if (!success) {
-//            //Directory creation failed
-//            //System.out.println("Directory creation failed. Папка уже создана?");
-//        }
-//
-//        Calendar calendar = new ApplicationManager().getCurrentCalendar();
-//        String SuccsessLogMessage =
-//                "The test - \"" +
-//                        result.getMethod().getMethodName().toString() +
-//                        "\" was successfully ended" +
-//                        "(" +
-//                        calendar.get(Calendar.DATE) +
-//                        "." +
-//                        (calendar.get(Calendar.MONTH) + 1) +
-//                        "." +
-//                        calendar.get(Calendar.YEAR) +
-//                        " " +
-//                        calendar.get(Calendar.HOUR_OF_DAY) +
-//                        ":" +
-//                        calendar.get(Calendar.MINUTE) +
-//                        ":" +
-//                        calendar.get(Calendar.SECOND) +
-//                        ")";
-//
-//        String ErrorLogMessage =
-//                "The test - \"" +
-//                        result.getMethod().getMethodName().toString() +
-//                        "\" was failed!" +
-//                        "(" +
-//                        calendar.get(Calendar.DATE) +
-//                        "." +
-//                        (calendar.get(Calendar.MONTH) + 1) +
-//                        "." +
-//                        calendar.get(Calendar.YEAR) +
-//                        " " +
-//                        calendar.get(Calendar.HOUR_OF_DAY) +
-//                        ":" +
-//                        calendar.get(Calendar.MINUTE) +
-//                        ":" +
-//                        calendar.get(Calendar.SECOND) +
-//                        ")";
-//
-//        try {
-//            if (!result.isSuccess()) {
-//                File screenshot1 = new File("TestReport/html/Screens/" + result.getMethod().getMethodName() + ".png");
-//                screenshot1.delete();
-//                File screenshotTempFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-//                try {
-//                    Files.copy(screenshotTempFile, screenshot1);
-//                } catch (Exception e) {
-//                    System.out.println(e);
-//                }
-//                Reporter.log(
-//                        "<center>Скриншот снят при падении теста " +
-//                                screenshot1.getName() +
-//                                ", URL = " +
-//                                driver.getCurrentUrl() +
-//                                "<br><div><a target=\"_blank\" href=\"Screens/" +
-//                                result.getMethod().getMethodName() +
-//                                ".png\"><img  style=\"height:400px; width: 600px;\"  src=\"" + "Screens/" +
-//                                result.getMethod().getMethodName() +
-//                                ".png" +
-//                                "\"></a></div><center><br><br>",
-//                        true);
-//                System.out.println(ErrorLogMessage);
-//            } else {
-//                System.out.println(SuccsessLogMessage);
-//                Reporter.log(SuccsessLogMessage);
-//            }
-//        } catch (Exception e) {
-//            new ApplicationManager().addErrorToTheReport("Connection with browser was lost.");
-//        }
-//    }
-
 }
