8ed54a59d22c478ee1f4e6dc619852da771a9e55
Dimiys <Dimiys@ua.FM>
2016-09-16 00:34:50 +0300

    Update spend_meeting_404.java

--- src/test/java/autoTests/TestSiute/spend_meeting_404.java
+++ src/test/java/autoTests/TestSiute/spend_meeting_404.java
@@ -33,7 +33,7 @@ public class spend_meeting_404 extends CustomMethods {
         FirefoxProfile profile = new FirefoxProfile();
         profile.setEnableNativeEvents(false);
         profile.setAcceptUntrustedCertificates(true);
-        profile.setPreference("WebDriver.load.strategy", "unstable")
+        profile.setPreference("WebDriver.load.strategy", "unstable");
 
 
         /********* Раскомментить для запуска на своем профиле и закоментить для дефолтного ***********/
