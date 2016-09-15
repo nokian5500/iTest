03d83220cf014bb3758ae4b5514e3f3e6ccbe0b6
Dimiys <Dimiys@ua.FM>
2016-09-16 00:30:23 +0300

    Update Test_Example_Select.java

--- src/test/java/autoTests/TestSiute/Test_Example_Select.java
+++ src/test/java/autoTests/TestSiute/Test_Example_Select.java
@@ -30,6 +30,8 @@ public class Test_Example_Select extends CustomMethods {
         FirefoxProfile profile = new FirefoxProfile();
         profile.setEnableNativeEvents(false);
         profile.setAcceptUntrustedCertificates(true);
+        profile.setPreference("WebDriver.load.strategy", "unstable")
+
 
         /********* Раскомментить для запуска на своем профиле и закоментить для дефолтного ***********/
 //        ProfilesIni allProfiles = new ProfilesIni();
