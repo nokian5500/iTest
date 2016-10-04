5ddcad24fb3a99e929c38c4847e0f325fabd3205
Dimiys <Dimiys@ua.FM>
2016-10-04 10:43:28 +0300

    Create start_job.sh

---
+++ start_job.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sParam=$(echo "baseurl = ")
+sUrl=$(cat /tmp/sHost.txt)
+sed -i  '$d'  src/test/resources/config.properties && echo $sParam $sUrl >> src/test/resources/config.properties
+sSource=/tmp/BPMN.txt
+if [ ! -f $sSource ]; then 
+echo "NOT FILE"
+sed "11 a <package name=\"autoTests.TestSiute\"/>" testng_simple.xml > testng_new.xml
+mv testng_new.xml testng.xml
+else
+action () {
+BPMN=$1
+if [ -e ./src/test/java/autoTests/TestSiute/${BPMN%.*}.java ]; then
+echo $BPMN
+sed "6 a <class name=\"autoTests.TestSiute.${BPMN%.*}\"/>" testng_simple.xml > testng_new.xml
+mv testng_new.xml testng_simple.xml
+cp testng_simple.xml testng.xml
+fi
+}
+fi
+
+
+while read sLine; do
+ action  $sLine
+ done < $sSource
+rm /tmp/BPMN.txt
+rm /tmp/sHost.txt
+exit 0
