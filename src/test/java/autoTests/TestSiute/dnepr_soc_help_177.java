caf11015b7842422d7c68a3d2cb21bbece39f3cb
GrekD <darja.grek@gmail.com>
2016-10-24 17:02:46 +0300

    Add autotest 177

---
+++ src/test/java/autoTests/TestSiute/dnepr_soc_help_177.java
@@ -0,0 +1,69 @@
+package autoTests.TestSiute;
+
+import autoTests.ConfigurationVariables;
+import autoTests.CustomMethods;
+import autoTests.pages.main.TemplatePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import ru.stqa.selenium.factory.WebDriverFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Privat24 on 09.09.2016.
+ */
+public class dnepr_soc_help_177 extends CustomMethods {
+
+    //<editor-fold desc="Тестовый пример заполнение полей">
+    @Test(enabled = true, groups = {"Main", "Критический функционал"}, priority = 1)
+    public void default_test() throws Exception {
+        /*****************************************объявляем элементы страниц*******************************************/
+        TemplatePage o = new TemplatePage(driver);
+        //  Вносим в переменные название услуги начиная с точки ._test_fields_bankid_--_ и до начала названия поля
+        String sBP = "dnepr_soc_help_177";
+        String email = "autotestbeta@gmail.com";
+
+        _step("1. Вход по прямому URL на услугу");
+        openURLservice(driver, CV.baseUrl + "/service/786/general");
+
+        _step("2. Проверить, что открылась нужная услуга");
+        assertThis(driver, o.usluga, "Видача довідок про отримання/неотримання усіх видів державних соціальних допомог");
+
+        _step("3. Выбор области/города");
+        o.selectRegion("Дніпропетровська");
+        
+        _step("4. Авторизация Off AuthMock/BankID");
+        o.mokAuthorization();
+
+        _step("5. Заполняем форму услуги");
+        setFieldAutocomplete(driver,"sID_Public_SubjectOrganJoin","Бабушкінській район, м.Дніпропетровськ");
+        
+        setFieldValue(driver, sBP, "adress1", "Дніпро");
+        setFieldValue(driver, sBP, "adress2", "Дніпро");
+        setFieldValue(driver, sBP, "phone", "+380102030405");
+        setFieldValue(driver, sBP, "email", "autotestbeta@gmail.com");
+        setFieldSelectByText(driver, sBP, "subekt", "особисто");
+        setFieldSelectByText(driver, sBP, "content", "призначення допомоги на дітей одиноким матерям");
+        setFieldValue(driver, sBP, "sMailClerk", "autotestbeta@gmail.com");
+        
+        _step("6. Отправка формы");
+        click(driver, o.buttonSendingForm);
+
+        _step("7. Проверка сообщения о успешной отправке");
+        o.checkMessageSuccess("Шановний(-а) MockUser MockUser!\n" +
+                "Ваше звернення х-хххххххх успішно зареєстровано\n" +
+                "(номер також відправлено Вам електронною поштою на Ваш e-mail "+email+") Результати будуть спрямовані також на email.\n" +
+                "Звертаємо увагу, що Іноді листи потрапляють у спам або у розділ \"Реклама\" (для Gmail).");
+
+        _step("8. Нажать кнопку Выйти");
+        click(driver, o.buttonLogOut);
+    }
+    //</editor-fold>
+}
