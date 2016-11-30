08fa457c8d2061b47365cfb9d204f3d7822ff08a
VadimGulich <vidokgulich@gmail.com>
2016-11-30 17:25:41 +0200

    import autoTests.API.DeleteTask;

--- src/test/java/autoTests/TestSiute/Test_Example_Attach.java
+++ src/test/java/autoTests/TestSiute/Test_Example_Attach.java
@@ -1,5 +1,6 @@
 package autoTests.TestSiute;
 
+import autoTests.API.DeleteTask;
 import autoTests.ConfigurationVariables;
 import autoTests.CustomMethods;
 import autoTests.pages.main.TemplatePage;
