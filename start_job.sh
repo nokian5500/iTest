adc0efd0537413c375a7ac0abbb8d544ee146954
Dimiys <Dimiys@ua.FM>
2016-12-13 11:07:23 +0200

    Update start_job.sh

--- start_job.sh
+++ start_job.sh
@@ -16,7 +16,7 @@ if [ -e ./src/test/java/autoTests/TestSiute/${BPMN%.*}.java ]; then
 echo $BPMN
 sed "6 a <class name=\"autoTests.TestSiute.${BPMN%.*}\"/>" testng_simple.xml > testng_new.xml
 mv testng_new.xml testng_simple.xml
-mv testng_simple.xml testng.xml
+cp testng_simple.xml testng.xml
 else 
 mv testng_simple.xml testng.xml
 fi
