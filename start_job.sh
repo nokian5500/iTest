f960fc8dd7f4b0ac97c9557b025932991c272450
Dimiys <Dimiys@ua.FM>
2016-12-16 13:57:19 +0200

    Update start_job.sh

--- start_job.sh
+++ start_job.sh
@@ -6,12 +6,12 @@ sSource=/tmp/BPMN.txt
 
 if [ ! -f $sSource ]; then 
 echo "NOT FILE"
-sed "11 a <!--package name=\"autoTests.TestSiute\"/>--" testng_simple.xml > testng_new.xml
+sed "11 a <package name=\"autoTests.TestSiute\"/>" testng_simple.xml > testng_new.xml
 echo "sed "11 a <package name=\"autoTests.TestSiute\"/>" testng_simple.xml > testng_new.xml"
 mv testng_new.xml testng.xml
 echo "mv testng_new.xml testng.xml WITH NO FILE"
 else
-echo "Add tests"
+#echo "Add tests"
 action () {
 BPMN=$1
 if [ -e ./src/test/java/autoTests/TestSiute/${BPMN%.*}.java ]; then
@@ -22,8 +22,8 @@ echo "mv testng_new.xml testng_simple.xml"
 cp testng_simple.xml testng.xml
 echo "cp testng_simple.xml testng.xml"
 else 
-cp testng_simple.xml testng.xml
-echo "cp testng_simple.xml testng.xml JUST"
+mv testng_simple.xml testng.xml
+echo "mv testng_simple.xml testng.xml JUST"
 fi
 }
 fi
