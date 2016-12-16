123b764775620d47ae0d73d4e332569bf40fa6b3
Dimiys <Dimiys@ua.FM>
2016-12-16 15:05:43 +0200

    Update start_job.sh

--- start_job.sh
+++ start_job.sh
@@ -7,32 +7,25 @@ sSource=/tmp/BPMN.txt
 if [ ! -f $sSource ]; then 
 echo "NOT FILE"
 sed "11 a <package name=\"autoTests.TestSiute\"/>" testng_simple.xml > testng_new.xml
-echo "sed "11 a <package name=\"autoTests.TestSiute\"/>" testng_simple.xml > testng_new.xml"
-mv testng_new.xml testng.xml
-echo "mv testng_new.xml testng.xml WITH NO FILE"
+cp testng_new.xml testng.xml
 else
-#echo "Add tests"
+echo "Add tests"
 action () {
 BPMN=$1
 if [ -e ./src/test/java/autoTests/TestSiute/${BPMN%.*}.java ]; then
 echo $BPMN
 sed "6 a <class name=\"autoTests.TestSiute.${BPMN%.*}\"/>" testng_simple.xml > testng_new.xml
 mv testng_new.xml testng_simple.xml
-echo "mv testng_new.xml testng_simple.xml"
 cp testng_simple.xml testng.xml
-echo "cp testng_simple.xml testng.xml"
 else 
-cp testng_simple.xml testng.xml
-echo "mv testng_simple.xml testng.xml JUST"
+mv testng_simple.xml testng.xml
 fi
 }
 fi
-
 
 
 while read sLine; do
  action  $sLine
  done < $sSource
 rm /tmp/BPMN.txt
-echo "rm /tmp/BPMN.txt"
 exit 0
