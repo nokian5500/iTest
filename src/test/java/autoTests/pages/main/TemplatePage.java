33272c8ea06168d79c4aca410d9e7ba1d92b71cc
HS <>
2017-02-08 20:59:42 +0200

    fix

--- src/test/java/autoTests/pages/main/TemplatePage.java
+++ src/test/java/autoTests/pages/main/TemplatePage.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.List;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.support.ui.Select;
 
 import static org.testng.Assert.assertEquals;
