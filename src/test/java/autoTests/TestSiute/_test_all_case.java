294e6238b9d6e99cd57a48e1d5d3602f4fceb1e7
Kirill.Smoktii <Kirill.Smoktii@SKV-PROBOOK>
2017-02-22 01:00:21 +0200

    Добавление теста

---
+++ src/test/java/autoTests/TestSiute/_test_all_case.java
@@ -0,0 +1,72 @@
+package autoTests.TestSiute;
+
+import autoTests.ConfigurationVariables;
+import autoTests.CustomMethods;
+import autoTests.pages.main.TemplatePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import ru.stqa.selenium.factory.WebDriverFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.apache.tools.ant.taskdefs.Sleep;
+
+/**
+ * Created by Privat24 on 09.09.2016.
+ */
+public class _test_all_case extends CustomMethods {
+
+
+    //<editor-fold desc="Тестовый пример заполнение полей">
+    @Test(enabled = true, groups = {"Main", "Критический функционал"}, priority = 1)
+   // Тест 1 - проверка Электронной очереди + основных типов полей + файлов
+    public void default_test1() throws Exception {
+        /*****************************************объявляем элементы страниц*******************************************/
+        TemplatePage o = new TemplatePage(driver);
+        //  Вносим в переменные название услуги начиная с точки ._test_fields_bankid_--_ и до начала названия поля
+        String sBP = "_test_all_case";
+        String email = "autotestbeta@gmail.com";
+
+        _step("1. Вход по прямому URL на услугу");
+        openURLservice(driver, CV.baseUrl + "/service/745/general");
+
+        // 2016-11-23 Убираем проверку на открытие нужной услуги.
+        // Считаем, что мы ее правильно по прямому пути выше 
+        // _step("2. Проверить, что открылась нужная услуга");
+        // assertThis(driver, o.usluga, "Копії рішень міської (селищної) ради про надання дозволу на розроблення проекту відведення земельної ділянки");
+
+        _step("3. Выбор области/города");
+        o.selectRegion("Вінницька");
+        o.selectCity("Вінниця");
+
+        _step("4. Авторизация Off AuthMock/BankID");
+        o.mokAuthorization();
+
+        _step("5. Заполняем форму услуги");
+        setFieldAutocomplete(driver,"sID_Public_SubjectOrganJoin","_test_all_case");
+        setFieldValue(driver, sBP, "phone", "+380623155533");
+        setFieldValue(driver, sBP, "email", email);
+        setFieldSelectSlotDate(driver, sBP, "."+sBP+"_--_"+"visitDay"); 
+        setFieldSelectSlotTime(driver, sBP, "."+sBP+"_--_"+"visitDay");
+        setFieldCheckBox(driver, sBP, "."+sBP+"_--_"+"#bMailWithSMS"); // #bMailWithSMS
+
+        _step("6. Отправка формы");
+        click(driver, o.buttonSendingForm);
+
+        _step("7. Проверка сообщения о успешной отправке");
+        o.checkMessageSuccess("Шановний(-а) MockUser MockUser!\n" +
+                "Ваше звернення х-хххххххх успішно зареєстровано\n" +
+                "(номер також відправлено Вам електронною поштою на Ваш e-mail "+email+") Результати будуть спрямовані також на email.\n" +
+                "Звертаємо увагу, що Іноді листи потрапляють у спам або у розділ \"Реклама\" (для Gmail).");
+
+        _step("8. Нажать кнопку Выйти");
+        click(driver, o.buttonLogOut);
+    }
+    //</editor-fold>
+}
