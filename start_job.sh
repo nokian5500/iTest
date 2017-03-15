fb55820bcde4961843be8cc8a481c5810b7d26a8
Dimiys <Dimiys@ua.FM>
2017-03-15 21:59:50 +0200

    Update start_job.sh

--- start_job.sh
+++ start_job.sh
@@ -32,8 +32,8 @@ sed -i  '$d'  src/test/resources/config.properties && echo $sParam $sUrl >> src/
 
 if [ ! -f $sSource ]; then 
 echo "NOT FILE"
-sed "11 a <package name=\"autoTests.TestSiute\"/>" testng_simple.xml > testng_new.xml
-cp testng_new.xml testng.xml
+#sed "11 a <package name=\"autoTests.TestSiute\"/>" testng_simple.xml > testng_new.xml
+#cp testng_new.xml testng.xml
 else
 echo "Add tests"
 action () {
@@ -43,8 +43,8 @@ echo $BPMN
 sed "6 a <class name=\"autoTests.TestSiute.${BPMN%.*}\"/>" testng_simple.xml > testng_new.xml
 mv testng_new.xml testng_simple.xml
 cp testng_simple.xml testng.xml
-else 
-mv testng_simple.xml testng.xml
+#else 
+#mv testng_simple.xml testng.xml
 fi
 }
 fi
