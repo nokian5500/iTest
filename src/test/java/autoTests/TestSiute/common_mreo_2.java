e9b7a30aa3a07cf518f20eefe525e8dfbd251dfa
Kirill.Smoktii <Kirill.Smoktii@SKV-PROBOOK>
2017-06-08 23:09:40 +0300

    common_mreo_2

---
+++ src/test/java/autoTests/TestSiute/common_mreo_2.java
@@ -0,0 +1,87 @@
+package autoTests.TestSiute;
+
+import autoTests.ConfigurationVariables;
+import autoTests.CustomMethods;
+import autoTests.pages.main.TemplatePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import ru.stqa.selenium.factory.WebDriverFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Privat24 on 09.09.2016.
+ */
+public class common_mreo_2 extends CustomMethods {
+
+    //<editor-fold desc="Тестовый пример заполнение полей">
+    @Test(enabled = true, groups = {"Main", "Критический функционал"}, priority = 1)
+    public void default_test() throws Exception {
+        /*****************************************объявляем элементы страниц*******************************************/
+        TemplatePage o = new TemplatePage(driver);
+        //  Вносим в переменные название услуги начиная с точки ._test_fields_bankid_--_ и до начала названия поля
+        String sBP = "common_mreo_2";
+        String email = "autotestbeta@gmail.com";
+
+        _step("1. Вход по прямому URL на услугу");
+        openURLservice(driver, CV.baseUrl + "/service/1397/general");
+
+        // 2016-11-23 Убираем проверку на открытие нужной услуги.
+        // Считаем, что мы ее правильно по прямому пути выше 
+        // _step("2. Проверить, что открылась нужная услуга");
+        // assertThis(driver, o.usluga, "Надання дозволу на знесення аварійних будівель");
+
+        _step("3. Выбор области/города");
+
+        _step("4. Авторизация Off AuthMock/BankID");
+        o.mokAuthorization();
+
+        _step("5. Заполняем форму услуги");
+        setFieldValue(driver, sBP, "bankIdbirthDay", "27.05.1985");
+        setFieldValue(driver, sBP, "phone", "+380623155533");
+        setFieldValue(driver, sBP, "email", email);
+        setFieldSelectByText(driver,sBP,"reqType","інформація про володіння транспортнними засобами фізичною особою");
+        setFieldSelectByText(driver,sBP,"reqPhis","пошук за індивідуальним податковим номером");
+        setFieldValue(driver, sBP, "ownerInn", "2731300834");
+        
+        _step("6. Отправка формы");
+        click(driver, o.buttonSendingForm);
+
+        _step("7. Проверка сообщения о успешной отправке");
+        o.checkMessageSuccess("Шановний(-а) MockUser MockUser!\n" +
+                "Ваше звернення х-хххххххх успішно зареєстровано\n" +
+                "(номер також відправлено Вам електронною поштою на Ваш e-mail "+email+") Результати будуть спрямовані також на email.\n" +
+                "Звертаємо увагу, що Іноді листи потрапляють у спам або у розділ \"Реклама\" (для Gmail).");
+
+        _step("8. Вход по прямому URL на дашборд");
+        openURLdashboard(driver, sBP);
+
+        _step("9. Авторизация login/BankID на дашборде. login/pass: (tester/tester)");
+        AuthorizationBySetLoginPassword(driver, sBP, "tester", "tester");
+        clickButton(driver, sBP, "Увійти");
+        // Опрацювання в табі "В необроблені"    
+        setRegionTask(driver, sBP);
+        clickButton(driver, sBP, "Взяти в роботу");
+        clickButton(driver, sBP, "Почати опрацювання задачі");
+
+        // Опрацювання [Етап I] 
+        SetRegionFieldInputTypeString(driver, sBP, "ownerFirstName", "Ім'я власника");
+        SetRegionFieldInputTypeString(driver, sBP, "ownerLastName", "Прізвище власника");
+        SetRegionFieldInputTypeString(driver, sBP, "ownerMiddleName", "По батькові власника");
+
+        clickButton(driver, sBP, "Опрацювати");
+        clickButton(driver, sBP, "Підтвердити");
+
+        _step("10. Нажать кнопку Выйти");
+        click(driver, o.buttonLogOut);
+        
+    }
+    //</editor-fold>
+}
