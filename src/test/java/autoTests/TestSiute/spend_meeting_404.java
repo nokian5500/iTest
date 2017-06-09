8a3ddba42f55fc876e507251bd206e92f3ab9e4e
Kirill.Smoktii <Kirill.Smoktii@SKV-PROBOOK>
2017-06-09 22:46:44 +0300

    fix2

--- src/test/java/autoTests/TestSiute/spend_meeting_404.java
+++ src/test/java/autoTests/TestSiute/spend_meeting_404.java
@@ -89,7 +89,8 @@ public class spend_meeting_404 extends CustomMethods {
         _step("9. Авторизация login/BankID на дашборде. login/pass: (tester/tester)");
         AuthorizationBySetLoginPassword(driver, sBP, "tester", "tester");
         clickButton(driver, sBP, "Увійти");
-     // Опрацювання в табі "В необроблені"    
+        
+        // Опрацювання в табі "В необроблені"    
         setRegionTask(driver, sBP);
         clickButton(driver, sBP, "Взяти в роботу");
         clickButton(driver, sBP, "Почати опрацювання задачі");
@@ -108,70 +109,13 @@ public class spend_meeting_404 extends CustomMethods {
         clickButton(driver, sBP, "Взяти в роботу");
         clickButton(driver, sBP, "Почати опрацювання задачі"); 
 
+        SetRegionFieldInputTypeEnum(driver, sBP, "decision_2", "повідомлення прийнято до відома");
+        SetRegionFieldInputTypeTextArea(driver, sBP, "comment_2", "коментар 2");
+
+        clickButton(driver, sBP, "Опрацювати");
+        clickButton(driver, sBP, "Підтвердити");
+        clickButton(driver, sBP, "Ok");
     }
-    /*
-    public void my_test2() throws Exception {
-        TemplatePage o = new TemplatePage(driver);
-        String sBP = "spend_meeting_404";
-        String email = "autotestbeta@gmail.com";
-
-        _step("1. Вход по прямому URL на услугу");
-        openURLservice(driver, CV.baseUrl + "/service/40/general");
-
-        _step("2. Проверить, что открылась нужная услуга");
-        assertThis(driver, o.usluga, "Повідомлення про проведення зборів, мітингів, маніфестацій і демонстрацій, спортивних, видовищних та інших масових заходів");
-
-        _step("3. Выбор области/города");
-        o.selectRegion("Київська");
-        o.selectCity("Колонщина");
-
-        _step("4. Авторизация Off AuthMock/BankID");
-        o.mokAuthorization();
-
-        _step("5. Заполняем форму услуги");
-        selectAutocomplete(driver,"sID_Public_SubjectOrganJoin","ЦНАП Колонщинської сільради");
-        setFieldValue(driver, sBP, "phone", "+380623155533");
-        setFieldValue(driver, sBP, "email", email);
-        setFieldValue(driver, sBP, "sMetaZahodu", "Мета заходу");
-        setFieldSelectByText(driver,sBP,"eFormaZahodu","мітинг");
-        setFieldValue(driver, sBP, "sMistoZahodu", "Місце проведення заходу або маршрути руху");
-        setFieldFile(driver, sBP, "fMapViewMarshrut", "src/test/resources/files/test.jpg");
-        setFieldValue(driver, sBP, "dDate_Beg", "2016-01-01");
-        setFieldSelectByText(driver,sBP,"eTime_Beg","12:00");
-        setFieldValue(driver, sBP, "dDate_End", "01.01.2016");
-        setFieldSelectByText(driver,sBP,"eTime_End","19:00");
-        setFieldValue(driver, sBP, "lCountPeople", "100");
-        setFieldFile(driver, sBP, "fPovidomlennyaProZahid", "src/test/resources/files/test.jpg");
-        setFieldSelectByText(driver,sBP,"eCountOrganizatorov","2");
-        setFieldValue(driver, sBP, "lastName_UA1", "Прізвище");
-        setFieldValue(driver, sBP, "firstName_UA1", "Ім'я");
-        setFieldValue(driver, sBP, "middleName_UA1", "По батькові");
-        setFieldValue(driver, sBP, "sAdress_1", "Адреса реєстрації відповідальної особи 1");
-        setFieldValue(driver, sBP, "sphone_1", "Контактний (особистий) номер");
-        setFieldValue(driver, sBP, "sPlace_of_Work_1", "Місце роботи (навчання) відповідальної особи 1");
-        setFieldSelectByText(driver,sBP,"isRepresentCompany_1","Так");
-        setFieldValue(driver, sBP, "sNameCompany_1", "Назва організації 1");
-        setFieldValue(driver, sBP, "sedrpou_1", "ЄДРПОУ організації 1");
-        setFieldValue(driver, sBP, "lastName_UA2", "Прізвище");
-        setFieldValue(driver, sBP, "firstName_UA2", "Ім'я");
-        setFieldValue(driver, sBP, "middleName_UA2", "По батькові");
-        setFieldValue(driver, sBP, "sAdress_2", "Адреса реєстрації відповідальної особи 2");
-        setFieldValue(driver, sBP, "sphone_2", "Контактний (особистий) номер 2");
-        setFieldValue(driver, sBP, "sPlace_of_Work_2", "Місце роботи (навчання) відповідальної особи 2");
-        setFieldSelectByText(driver,sBP,"isRepresentCompany_2","Ні");
-
-        _step("6. Отправка формы");
-        click(driver, o.buttonSendingForm);
-
-        _step("7. Проверка сообщения о успешной отправке");
-        o.checkMessageSuccess("Шановний(-а) MockUser MockUser!\n" +
-                "Ваше звернення х-хххххххх успішно зареєстровано\n" +
-                "(номер також відправлено Вам електронною поштою на Ваш e-mail "+email+") Результати будуть спрямовані також на email.\n" +
-                "Звертаємо увагу, що Іноді листи потрапляють у спам або у розділ \"Реклама\" (для Gmail).");
-
-        _step("8. Нажать кнопку Выйти");
-        click(driver, o.buttonLogOut);
-    } */
-
+ 
     //</editor-fold>
 }
