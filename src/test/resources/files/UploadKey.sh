4b52f0b9994cf79a36a4b8802af1ff76ccf04e2a
Danil Gorobets <danilgorobec4@gmail.com>
2017-11-14 16:31:44 +0200

    clear

--- src/test/resources/files/UploadKey.sh
+++
@@ -1,3 +0,0 @@
-ControlFocus("Оберiть файл з ключами ЕЦП","","Edit1")
-ControlSetText("Оберiть файл з ключами ЕЦП","","Edit1","src\test\resources\files\Key-6.dat")
-ControlClick("Оберiть файл з ключами ЕЦП", "","Button2");
