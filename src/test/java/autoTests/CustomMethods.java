dfe201759b4105a6b89a2c4a1463857f0e131044
bandera <dnepryanin18@gmail.com>
2018-03-16 10:46:28 +0200

    revert

--- src/test/java/autoTests/CustomMethods.java
+++ src/test/java/autoTests/CustomMethods.java
@@ -542,8 +542,8 @@ public class CustomMethods extends SetupAndTeardown {
      * @param nameButton
      */
     public void clickButton(String nameButton) { // нажатие любой кнопки с указанным тескстом на ней
-        //$(By.xpath("//button[contains(.,'" + nameButton + "')]")).click(); ////button[contains(.,'Опрацювати')]
-        $x("//button").should(text(nameButton)).click();
+        $(By.xpath("//button[contains(.,'" + nameButton + "')]")).click(); ////button[contains(.,'Опрацювати')]
+        //$x("//button").shouldHave(text(nameButton)).click();
     }
 
     /**
