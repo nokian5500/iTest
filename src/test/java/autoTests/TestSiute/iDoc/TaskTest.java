d09bd49f7aba1f4b17ce9b1a23c2e435935ee9fc
Oleksandr Gurtovyi <gurtik7@gmail.com>
2018-07-20 09:55:30 +0300

    edit TaskTest https://idoc-develop.atlassian.net/browse/TEST-39

--- src/test/java/autoTests/TestSiute/iDoc/TaskTest.java
+++ src/test/java/autoTests/TestSiute/iDoc/TaskTest.java
@@ -228,4 +228,46 @@ public class TaskTest extends CustomMethods{
 
         System.out.println(boolean2);
     }
+
+    @Test
+    public void testUrgentButton() throws Exception {
+        String LoginCollective1 = "IGOV_160582SOD";
+        String NameCollective1 = "Смоктій Оксана Данилівна";
+        String LoginCollective2 = "IGOV_310780BVV";
+        String NameCollective2 = "Белявцев Володимир Володимирович";
+        String LoginCollective4 = "IGOV_100982SOV";
+        String NameCollective4 = "Смірнова Олена Володимирівна";
+        String sBP = "_doc_btsol_vertical_sz";
+        openURLdashboard(getRegionUrl());
+        AuthorizationBySetLoginPassword("ZCPK_031260SVM", " ");
+        pause(3000);
+        navigateToggleMenu();
+        setRegionFindOrder("14-35973250");
+        pause(4000);
+        clickButton("Інші дії");
+        pause(1000);
+        clickButton("Додати на перегляд");
+       pause(1000);
+       prostoMetodSchaUdoly("Давидчак Ярослав Романович");
+        pause(1000);
+        checkPositionNotNull();
+        pause(8000);
+    }
 }
+    /*String LoginAuthor = "ZCPK_310767TVV";
+    String NameAuthor = "Терентьєв Володимир Володимирович";
+    String Login1 = "ZCPK_020379CDP";
+    String Name1 = "Чмихал Дмитро Павлович";
+    String Login2 = "ZCPK_150960POV";
+    String Name2 = "Пітула Олександр Володимирович";
+    String Login3 = "ZCPK_050991BSO";
+    String Name3 = "Будай Coломія Олексіївна";
+    String Login4 = "ZCPK_280562DGI";
+    String Name4 = "Долінська Галина Йосипівна";
+    String Login5 = "ZCPK_220185NSV";
+    String Name5 = "Норов Станіслав Валентинович";
+    String Login6 = "ZCPK_230161DYR";
+    String Name6 = "Давидчак Ярослав Романович";
+    String Login7 = "ZCPK_031260SVM";
+    String Name7 = "Стефанів Василь Михайлович";*/
+
